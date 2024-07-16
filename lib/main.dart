7440c8431a2f366c994b3cd3d24fdf1d6d051026
st1llsane <notescape262@gmail.com>
2024-07-17 00:57:57 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
